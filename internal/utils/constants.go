96a08cf733eda8601af4ac1a9637ecb3e3a832d0
arkn98 <20590666+arkn98@users.noreply.github.com>
2020-06-01 17:43:32 +0530

    Remove repeating strings in constants.go

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -1,12 +1,28 @@
 package utils
 
 const (
+	// EnvPrefix is used as the prefix for all env variables related to adeia.
+	EnvPrefix = "ADEIA"
+
+	// ==========
+	// Keys of env variables to override config from config.yaml
+	// ==========
+
 	// EnvConfPathKey is the env key for confPath.
-	EnvPrefix        = "ADEIA"
-	EnvConfPathKey   = "ADEIA_CONF_PATH"
-	EnvDBNameKey     = "ADEIA_DB_NAME"
-	EnvDBUserKey     = "ADEIA_DB_USER"
-	EnvDBPasswordKey = "ADEIA_DB_PASSWORD"
-	EnvDBHostKey     = "ADEIA_DB_HOST"
-	EnvDBPortKey     = "ADEIA_DB_PORT"
+	EnvConfPathKey = EnvPrefix + "_CONF_PATH"
+
+	// EnvDBNameKey is the env key for database name.
+	EnvDBNameKey = EnvPrefix + "_DB_NAME"
+
+	// EnvDBUserKey is the env key for database user.
+	EnvDBUserKey = EnvPrefix + "_DB_USER"
+
+	// EnvDBPasswordKey is the env key for database password.
+	EnvDBPasswordKey = EnvPrefix + "_DB_PASSWORD"
+
+	// EnvDBHostKey is the env key for database host.
+	EnvDBHostKey = EnvPrefix + "_DB_HOST"
+
+	// EnvDBPortKey is the env key for database port.
+	EnvDBPortKey = EnvPrefix + "_DB_PORT"
 )
